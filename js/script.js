230159eed034e0b2f85e891ebe40b7dfd7588e04
IanM21 <ianmcdonad2003@gmail.com>
2023-05-20 19:54:43 -0400

    Updated Navbar + Mobile Responsiveness

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
 const currentYear = new Date().getFullYear();
 const footer = document.querySelector("footer");
 footer.innerHTML = `Copyright © ${currentYear} Ian McDonald`;
+
+// NavBar
+const primaryNav = document.querySelector('.primary-navbar');
+const navToggle = document.querySelector('.mobile-nav-toggle');
+
+navToggle.addEventListener('click', () => {
+    const visibility = primaryNav.getAttribute('data-visible');
+
+    if (visibility === 'false') {
+        primaryNav.setAttribute('data-visible', 'true');
+        navToggle.setAttribute('aria-expanded', 'true');
+    }
+    else {
+        primaryNav.setAttribute('data-visible', 'false');
+        navToggle.setAttribute('aria-expanded', 'false');
+    }
+    }
+);
